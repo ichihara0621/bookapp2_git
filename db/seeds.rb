aacee982b77c1804f3d1fb579c407ac16223a317
ichihara <ichihara.misato@lmi.ne.jp>
2019-10-19 07:27:27 +0000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create!(title:  "Example User",
+             author: "example@railstutorial.org",
+             publisher: "Example",
+             genre: "Ex")
+
+39.times do |n|
+  title  = Faker::Book.title
+  author = Faker::Book.author
+  publisher = Faker::Book.publisher
+  genre = Faker::Book.genre
+  Book.create!(title:  title,
+               author: author,
+               publisher:   publisher,
+               genre: genre)
+end
