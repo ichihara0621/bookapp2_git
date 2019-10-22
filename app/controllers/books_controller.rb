c9368fc841e720c5f72bb60b52db9b373a887e2f
ichihara <ichihara.misato@lmi.ne.jp>
2019-10-22 06:45:39 +0000

    session

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+    before_action :logged_in_user
+
   def show
     @books = Book.find(params[:id])
   end
@@ -53,5 +55,12 @@ class BooksController < ApplicationController
 
   def book_params
      params.require(:book).permit(:title, :author, :genre)
+  end
+
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
   end
 end
