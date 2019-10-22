c50734cef01d56ff6467bfc808cd1ea4fe007e6d
ichihara <ichihara.misato@lmi.ne.jp>
2019-10-22 09:51:54 +0000

    rememberlost

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+       log_in user
       redirect_to user
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      # エラーメッセージを作成する
-     flash.now[:danger] = 'Invalid email/password combination'
-     render 'new'
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
 
-  def destroy
-    log_out if logged_in?
+ def destroy
+    log_out
     redirect_to root_url
   end
 end
